868c775c9ed69a67485b36980b9a9672f97affaa
Your Name <you@example.com>
2025-07-30 21:13:27 +0530

    modify the env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5165',
-  signalRUrl: 'https://localhost:5165/chathub'
-};
+
+  /*apiUrl: 'https://localhost:5165',
+  signalRUrl: 'https://localhost:5165/chathub' */
+  apiUrl: 'https://chat-app-api-8vam.onrender.com',
+  signalRUrl: 'https://chat-app-api-8vam.onrender.com/chathub',
+};  
